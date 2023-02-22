aa892437ad234e3c5c76c683b46d945a32b4f118
pawelprojext <mailkaliego@gmail.com>
2023-02-22 10:09:33 +0100

    responsive done, js updated

--- FAQ/index.js
+++ FAQ/index.js
@@ -1,13 +1,18 @@
 const questions = document.getElementById("questions");
-const arrow = document.querySelector('img');
+const arrow = document.querySelectorAll('.ToogleArrow');
+
 
 for(const question of questions.children){
     for(const answer of question.children){
         if(!answer.classList.contains('question')){
             question.addEventListener("click", ()=>{
                 answer.classList.toggle('hide');
-                answer.classList.toggle('bnone');
+                question.classList.toggle('blackText');
+                arrow.forEach(function (button){
+                    button.classList.toggle('arrow');
+                });
             });
+            
         }
     }  
 }
