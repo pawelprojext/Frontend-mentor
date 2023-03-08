666df473b5de31c84c89d0d0e9bb5a4dc6c6d41b
pawelprojext <mailkaliego@gmail.com>
2023-03-08 10:23:28 +0100

    task done

--- blogr/index.js
+++ blogr/index.js
@@ -1,7 +1,18 @@
 const drops = document.querySelectorAll('.open');
+const mobileOpenBtn = document.querySelector('.nav__mobile-open-btn');
+const mobileContainer = document.querySelector('.nav__dropdown-containers');
+const mobileContent = document.querySelector('.nav__dropdown');
+const mobileDropContent = document.querySelector('.nav__dropdown-content');
+
 drops.forEach((drop) =>{
     drop.addEventListener("click", ()=>{
         drop.parentElement.lastElementChild.classList.toggle("hide");
         drop.parentElement.lastElementChild.classList.toggle("show");
     })
+});
+
+mobileOpenBtn.addEventListener("click", ()=>{
+    mobileContainer.classList.toggle('nav__mobile-containers');
+    mobileContent.classList.toggle('nav__mobile-container');
+    mobileContainer.classList.toggle('nav__mobile-dropdown-content');
 });
