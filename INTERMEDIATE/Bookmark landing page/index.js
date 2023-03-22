78b30d217873b9aecb3141b2e4f7107c98c8afa8
pawelprojext <mailkaliego@gmail.com>
2023-03-22 19:58:41 +0100

    task done

--- INTERMEDIATE/Bookmark landing page/index.js
+++ INTERMEDIATE/Bookmark landing page/index.js
@@ -6,6 +6,13 @@ const main = document.querySelector("main");
 
 const featuresAnswers = document.querySelectorAll(".feature-item");
 
+const faqQuestion = document.querySelectorAll(".faq-question");
+
+const Input = document.querySelector("input");
+const InputValue = Input.value;
+const sendBtn = document.querySelector(".input-btn");
+
+//navbar
 navToggle.addEventListener("click", ()=>{
     let visiblity = navList.getAttribute("data-visible");
     
@@ -21,6 +28,8 @@ navToggle.addEventListener("click", ()=>{
         navToggle.setAttribute("data-expanded", false);
     }
 });
+
+//features
 
 function hideActive(){
     const activeElement =  document.querySelector(".active-paragraph");
@@ -39,4 +48,23 @@ featuresAnswers.forEach((e, i)=>{
         e.classList.toggle("active-paragraph");
         document.getElementById("tab"+tabs).classList.toggle("feature-active");
     })  
+});
+
+//faq
+
+faqQuestion.forEach((e,i)=>{
+
+    e.addEventListener("click", ()=>{
+        let visiblity = e.parentElement.lastElementChild.getAttribute("data-visible");
+        if (visiblity === "false"){
+            e.parentElement.lastElementChild.setAttribute("data-visible", true);
+            e.lastElementChild.setAttribute("data-expanded", "exp");
+            
+        } else{
+            e.parentElement.lastElementChild.setAttribute("data-visible", false);
+            e.lastElementChild.setAttribute("data-expanded", "hidden");
+        }
+
+        
+    });
 });
